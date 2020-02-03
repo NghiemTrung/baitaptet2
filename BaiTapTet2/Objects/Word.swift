d8294e7f4d1ecfdbc28621d3659e9ff043c9064a
Trung Nghiem <nghiem.duc.trung@gmail.com>
2020-02-04 06:49:41 +0700

    first commit

---
+++ BaiTapTet2/Objects/Word.swift
@@ -0,0 +1,15 @@
+//
+//  Word.swift
+//  BaiTapTet2
+//
+//  Created by Trung on 2/2/20.
+//  Copyright © 2020 Trung. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class Word {
+    string wordLabel
+    
+}
---
+++ BaiTapTet2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BaiTapTet2
+//
+//  Created by Trung on 2/2/20.
+//  Copyright © 2020 Trung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
